8ae7471a3699ec21dce4740a15a86c532021d04b
emilyphalperin@gmail.com <emilyhalperin@Emilys-MBP-2.lan>
2023-07-19 10:38:32 -0400

    add invoice data to each pdf

--- main.py
+++ main.py
@@ -11,10 +11,18 @@ for filepath in filepaths:
     #print(df)
     pdf = FPDF(orientation='P', unit='mm', format='A4')
     pdf.add_page()
+
     filename = Path(filepath).stem
     invoice_nr = filename.split('-')[0]
+    date= filename.split('-')[1]
+
     pdf.set_font(family='Times', size=16, style='B')
-    pdf.cell(w=50, h=8, txt=f'Invoice nr.{invoice_nr}')
+    pdf.cell(w=50, h=8, txt=f'Invoice nr.{invoice_nr}', ln=1)
+
+    pdf.set_font(family='Times', size=16, style='B')
+    pdf.cell(w=50, h=8, txt=f'Date: {date}')
+
+
     pdf.output(f'PDFS/{filename}.pdf')
 
     
